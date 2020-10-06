709ccc0eb3d767396583d518eedfb68c492c4665
guil24 <61526290+guil24@users.noreply.github.com>
2020-10-05 22:27:48 -0300

    Add files via upload

---
+++ app/src/main/java/com/guillermoandroid/mi_primer_proyecto_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.guillermoandroid.mi_primer_proyecto_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
